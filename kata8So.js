77cf240ea27e15c90c04a5654444aec23226e30a
Mikalai <mikalai.mikhalachkin@gmail.com>
2022-11-06 16:41:47 +0100

    bmi

--- kata8So.js
+++ kata8So.js
@@ -5,3 +5,16 @@ function sum (numbers) {
     else {return numbers.reduce((total, currentValue) => total + currentValue);}
 
 };
+
+function bmi(weight, height) {
+    var bmi = weight / (height*height);
+    if (bmi <= 18.5) {
+        return "Underweight";
+    } else if (bmi <= 25) {
+        return "Normal";
+    } else if (bmi <= 30) {
+        return "Overweight";
+    } else if (bmi > 30) {
+        return "Obese";
+    }
+}
