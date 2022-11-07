146497580a6768ee177d8805851108ef8bddc5d1
Mikalai <mikalai.mikhalachkin@gmail.com>
2022-11-07 04:43:46 +0100

    finalGrade

--- kata8Mo.js
+++ kata8Mo.js
@@ -10,3 +10,10 @@ function isPalindrome(x) {
     // your code here
     return x.toLowerCase() == x.toLowerCase().split('').reverse().join('');
 }
+
+function finalGrade (exam, projects) {
+    return exam > 90 || projects > 10 ? 100
+        : exam > 75 && projects > 4 ? 90
+            : exam > 50 && projects > 1 ? 75
+                : 0
+}
